35b7754bb5fda216e41815ee69e6c3551589f797
florin <florinasca@mail.ru>
2025-05-20 18:19:29 +0300

    Initial commit.

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static jdk.internal.org.jline.utils.Colors.s;
+
 @Controller
 public class UserController {
     @Autowired private UserService service;
@@ -13,6 +15,12 @@ public class UserController {
     public String showUsersList(Model model) {
         service.listAll();
         return "users";
+    }
+
+    @GetMapping( "/users/new")
+    public String showNewUserForm(Model model) {
+        model.addAttribute(s: users, new Users());
+        return "user_form";
     }
 
 }
